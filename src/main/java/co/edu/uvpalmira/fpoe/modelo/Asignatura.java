94cd79abd66ec8e8c45e6e366a9e7b7d8032e36f
David Rendon <oo7davidrendon@gmail.com>
2026-05-20 18:02:37 -0500

    Coneccion con la base de datps y creacion de test para la clase asignatura

--- src/main/java/co/edu/uvpalmira/fpoe/modelo/Asignatura.java
+++ src/main/java/co/edu/uvpalmira/fpoe/modelo/Asignatura.java
@@ -4,19 +4,36 @@
  */
 package co.edu.uvpalmira.fpoe.modelo;
 
+import co.edu.uvpalmira.fpoe.jpalib.AbstractEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 
 /**
  *
  * @author DavidRendon
  */
 @Entity
-public class Asignatura {
+@NamedQueries({
+    @NamedQuery(name = "Asignatura.buscarCodigo",
+            query = "SELECT a FROM Asignatura a WHERE A.codigo = :codigo") ,
+    @NamedQuery(name = "Asignatura.buscarPorEscuela",
+            query = "SELECT a FROM Asignatura a WHERE a.codigo LIKE :prefijo ORDER BY a.codigo ASC")
+})
+public class Asignatura extends AbstractEntity {
     
+    @Column(nullable = false, unique = true, length = 7)
     String codigo;
+    @Column(nullable = false, unique = false, length = 150)
     String nombre;
+    @Column(nullable = false)
     byte creditos;
+    @Column(nullable = false)
     byte intensidad;
+
+    public Asignatura() {
+    }
 
     public Asignatura(String codigo, String nombre, byte creditos, byte intensidad) {
         this.codigo = codigo;
@@ -41,4 +58,20 @@ public class Asignatura {
         return intensidad;
     }
 
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCreditos(byte creditos) {
+        this.creditos = creditos;
+    }
+
+    public void setIntensidad(byte intensidad) {
+        this.intensidad = intensidad;
+    }
+    
 }
---
+++ src/test/java/co/edu/uvpalmira/fpoe/persistencia/AsignaturaDAOTest.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package co.edu.uvpalmira.fpoe.persistencia;
+
+import co.edu.uvpalmira.fpoe.modelo.Asignatura;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sala Sistemas
+ */
+public class AsignaturaDAOTest {
+    
+    private AsignaturaDAO asignaturaDAO;
+    private Long id ;
+    
+    public AsignaturaDAOTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PaginaWeb1PU");
+        this.asignaturaDAO = new AsignaturaDAO(emf) ;
+    }
+    
+    /**
+     * Test of buscarPorCodigo method, of class AsignaturaDAO.
+     */
+    @Test
+    public void testBuscarPorCodigo() {
+        try {
+            System.out.println("Insertar");
+            Asignatura asig = new Asignatura("750014C", "FPOE", (byte)3, (byte)3);
+            asignaturaDAO.create(asig);
+            this.id = asig.getId();
+            System.out.println(this.id);
+            Assert.assertNotNull(asig.getId());
+            
+            System.out.println("Buscar");
+            asig = null;
+            asig = asignaturaDAO.buscarPorCodigo("750014C");
+            Assert.assertEquals("FPOE", asig.getNombre());
+            
+            asig.setNombre("Fund. Prog. O. Eventos");
+            this.asignaturaDAO.edit(asig);
+            
+            asig = asignaturaDAO.buscarPorCodigo("750014C");
+            Assert.assertEquals("Fund. Prog. O. Eventos", asig.getNombre());
+            
+        } catch (Exception ex) {
+            Logger.getLogger(AsignaturaDAOTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+}
